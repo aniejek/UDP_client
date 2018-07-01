f7e89675a4c336ad152f0f97379dcb2c1bd47377
aniejek <nowywisniowy@gmail.com>
2018-07-01 13:06:58 +0200

    optimize drawing

--- display.py
+++ display.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from time import clock
 from random import randint
+from random import random as rand
 
 STEP = 30
 FPS = 50  # int(input('podaj ilość klatek na sekundę:\n'))
@@ -12,24 +13,30 @@ class Rect:
     LEFT = 2
     RIGHT = 3
 
-    def __init__(self, x, y, w, h, c, s, screen):
+    def __init__(self, x=None, y=None, width=None, height=None, c=None, speed=None, screen=None):
+        x = x if x is not None else randint(0, 300)
+        y = y if y is not None else randint(0, 100)
+        width = width if width is not None else randint(30, 50)
+        height = height if height is not None else randint(30, 50)
+        c = c if c is not None else (randint(0, 255), randint(0, 255), randint(0, 255))
+        speed = speed if speed is not None else 1 + 2 * rand()
         self.x = x
         self.y = y
-        self.w = w
-        self.h = h
+        self.width = width
+        self.height = height
         self.c = c
         self.__speed__ = speed
         self.__screen__ = screen
 
     def draw(self):
-        pg.draw.rect(screen, self.c, pg.Rect(
-            self.x, self.y, self.w, self.h))
+        pg.draw.rect(self.__screen__, self.c, pg.Rect(
+            self.x, self.y, self.width, self.height))
 
     def set_color(self, r, g, b):
         self.c = (r, g, b)
 
-    def set_height(self, h):
-        self.h = h
+    def set_height(self, height):
+        self.height = height
 
     def set_x(self, x):
         self.x = x
@@ -49,18 +56,32 @@ class Rect:
             self.x -= scope
 
 
-pg.init()
-_list = []
-screen = pg.display.set_mode((400, 300))
-# for i in range(randint(5, 10)):
-x = randint(0, 300)
-y = randint(0, 100)
-w = randint(30, 50)
-h = randint(30, 50)
-c = (randint(0, 255), randint(0, 255), randint(0, 255))
-speed = 2#00.0 / randint(100, 500)
-my_rect = Rect(x, y, w, h, c, speed, screen)
-_list.append(my_rect)
+class State:
+    def __init__(self, width=400, height=300):
+        self.objects = []
+        self.width = width
+        self.height = height
+        self.screen = None
+
+    def update(self, frame: bytes):
+        for object, byte in zip(self.objects, frame):
+            object.update(byte)
+
+    @classmethod
+    def get_initial_state(cls) -> 'State':
+        state = cls()
+        pg.init()
+        state.screen = pg.display.set_mode((state.width, state.height))
+        return state
+
+    def add(self, object):
+        object.screen = self.screen
+        self.objects.append(object)
+
+
+v_dir = 0
+h_dir = 0
+state = State.get_initial_state()
 done = False
 t = clock()
 frame = clock()
@@ -79,26 +100,25 @@ while not done:
     # counting time delta
     delta = STEP*(clock() - frame)
     frame = clock()
-
+    v_dir = 0
+    h_dir = 0
     # up
     if keys[pg.K_UP]:
-        my_rect.y -= delta * my_rect.get_speed()
+        v_dir += -1
     # down
     if keys[pg.K_DOWN]:
-        my_rect.y += delta * my_rect.get_speed()
+        v_dir += 1
     # left
     if keys[pg.K_LEFT]:
-        my_rect.x -= delta * my_rect.get_speed()
+        h_dir += -1
     # right
     if keys[pg.K_RIGHT]:
-        my_rect.x += delta * my_rect.get_speed()
+        h_dir += 1
 
     if (clock() - t) > (1.0 / FPS):
         print(i)
         i = 0
         t += 1.0 / FPS
-        screen.fill((0, 0, 0))
-        for rect in _list:
-            rect.draw()
+        state.update(bytes(10))
 
     pg.display.flip()
